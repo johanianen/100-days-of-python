7c9b111482c8199fbc9231cc9bc9262af2b6d089
johanianen <johan.kachiun@gmail.com>
2023-01-27 16:39:49 +0100

    us states game mabye finished

---
+++ us-states-game-start/error.py
@@ -0,0 +1,3 @@
+ERROR_POSITION = (0, 200)
+ERROR_FONT = ("consolas", 20)
+ERROR_SLEEPTIME = 2
---
+++ us-states-game-start/main.py
@@ -0,0 +1,76 @@
+import turtle
+import pandas
+import numpy
+import time
+
+ERROR_POSITION = (0, 200)
+ERROR_FONT = ("consolas", 20)
+ERROR_SLEEPTIME = 2
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+screen.setup(725, 491)
+image = "./blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+text = turtle.Turtle()
+text.penup()
+text.hideturtle()
+
+error_text = turtle.Turtle()
+error_text.penup()
+error_text.hideturtle()
+error_text.color("red")
+
+states_data = pandas.read_csv("./50_states.csv")
+states_list = states_data["state"].to_list()
+correct_guesses = []
+wrong_guesses = []
+strikes = 0
+score = len(correct_guesses)
+
+while True:
+    answer = screen.textinput(title="Guess the States", prompt="Type the name of a U.S. state")
+    answer = answer.title()
+    if answer not in states_list:
+        strikes += 1
+        wrong_guesses.append(answer)
+        error_text.goto(ERROR_POSITION)
+        error_text.write("Nope, that's not a U.S. State", align="center", font=ERROR_FONT)
+        time.sleep(ERROR_SLEEPTIME)
+
+    elif answer in correct_guesses:
+        strikes += 1
+        error_text.goto(ERROR_POSITION)
+        error_text.write("You've already guessed that state", align="center", font=ERROR_FONT)
+        time.sleep(ERROR_SLEEPTIME)
+
+
+    elif answer in wrong_guesses:
+        strikes += 3
+        error_text.goto(ERROR_POSITION)
+        error_text.write("I already told you once this wasn't a U.S. State! You lose!", align="center", font=ERROR_FONT)
+        time.sleep(ERROR_SLEEPTIME)
+
+    else:
+        xcor = states_data.loc[states_data["state"] == answer, "x"].item()
+        ycor = states_data.loc[states_data["state"] == answer, "y"].item()
+        print(xcor)
+        print(ycor)
+        
+        text.goto(xcor, ycor)
+        text.write(answer, align="center")
+
+        correct_guesses.append(answer)
+        
+    if strikes >= 3:
+        break
+
+rgb_weights = [0.2989, 0.5870, 0.1140]
+grayscale_image = numpy.dot(image[...,:3], rgb_weights)
+screen.addshape(image)
+turtle.shape(image)
+
+turtle.mainloop()
+
